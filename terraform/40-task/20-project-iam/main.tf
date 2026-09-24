92e5a846a734a54c68745488552ce8dd23efe5ab
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-25 08:24:21 +0900

    Add terraform/40-task/20-project-iam/main.tf

---
+++ terraform/40-task/20-project-iam/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">= 1.5.7"
+  required_providers { google = { source = "hashicorp/google", version = "~> 6.0" } }
+}
+variable "project_id" { type = string }
+variable "group_email" { type = string }
+provider "google" { project = var.project_id }
+resource "google_project_iam_member" "query_job_user" {
+  project = var.project_id
+  role = "roles/bigquery.jobUser"
+  member = "group:${var.group_email}"
+}
