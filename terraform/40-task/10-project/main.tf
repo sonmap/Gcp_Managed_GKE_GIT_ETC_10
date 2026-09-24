fac7db83809bcd99fe451ad6504ec840157a12b6
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-25 08:25:00 +0900

    Normalize HCL syntax in terraform/40-task/10-project/main.tf

--- terraform/40-task/10-project/main.tf
+++ terraform/40-task/10-project/main.tf
@@ -3,8 +3,10 @@ terraform {
   required_providers { google = { source = "hashicorp/google", version = "~> 6.0" } }
 }
 variable "project_id" { type = string }
-variable "region" { type = string; default = "asia-northeast3" }
-provider "google" { project = var.project_id; region = var.region }
+variable "region" { type = string
+  default = "asia-northeast3" }
+provider "google" { project = var.project_id
+  region = var.region }
 locals { services = toset(["bigquery.googleapis.com", "storage.googleapis.com", "iam.googleapis.com"]) }
 resource "google_project_service" "task" {
   for_each = local.services
