9683c985c4bee9f7068071ba5b501c3ff60aa865
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-25 08:24:20 +0900

    Add terraform/40-task/10-project/main.tf

---
+++ terraform/40-task/10-project/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.5.7"
+  required_providers { google = { source = "hashicorp/google", version = "~> 6.0" } }
+}
+variable "project_id" { type = string }
+variable "region" { type = string; default = "asia-northeast3" }
+provider "google" { project = var.project_id; region = var.region }
+locals { services = toset(["bigquery.googleapis.com", "storage.googleapis.com", "iam.googleapis.com"]) }
+resource "google_project_service" "task" {
+  for_each = local.services
+  project = var.project_id
+  service = each.value
+  disable_on_destroy = false
+}
+output "project_id" { value = var.project_id }
