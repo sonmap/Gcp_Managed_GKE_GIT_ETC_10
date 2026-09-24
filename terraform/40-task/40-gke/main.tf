50d3de41fc5e760a516d08624ef4a8d3eddfd891
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-25 08:25:04 +0900

    Normalize HCL syntax in terraform/40-task/40-gke/main.tf

--- terraform/40-task/40-gke/main.tf
+++ terraform/40-task/40-gke/main.tf
@@ -5,13 +5,18 @@ terraform {
     kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.0" }
   }
 }
-variable "gke_project_id" { type = string; default = "gcp-sbx-edp-gke01" }
-variable "cluster_name" { type = string; default = "gke-sbx-edp-main-an3" }
-variable "location" { type = string; default = "asia-northeast3" }
+variable "gke_project_id" { type = string
+  default = "gcp-sbx-edp-gke01" }
+variable "cluster_name" { type = string
+  default = "gke-sbx-edp-main-an3" }
+variable "location" { type = string
+  default = "asia-northeast3" }
 variable "task_name" { type = string }
 variable "jupyter_gsa_email" { type = string }
 data "google_client_config" "current" {}
-data "google_container_cluster" "main" { project = var.gke_project_id; name = var.cluster_name; location = var.location }
+data "google_container_cluster" "main" { project = var.gke_project_id
+  name = var.cluster_name
+  location = var.location }
 provider "kubernetes" {
   host = "https://${data.google_container_cluster.main.endpoint}"
   token = data.google_client_config.current.access_token
@@ -26,10 +31,13 @@ resource "kubernetes_service_account_v1" "jupyter" {
   }
 }
 resource "kubernetes_resource_quota_v1" "task" {
-  metadata { name = "quota-${var.task_name}"; namespace = kubernetes_namespace_v1.task.metadata[0].name }
+  metadata { name = "quota-${var.task_name}"
+  namespace = kubernetes_namespace_v1.task.metadata[0].name }
   spec { hard = { "requests.cpu" = "40", "requests.memory" = "320Gi", "requests.storage" = "3Ti", "persistentvolumeclaims" = "100" } }
 }
 resource "kubernetes_network_policy_v1" "default_deny_ingress" {
-  metadata { name = "default-deny-ingress"; namespace = kubernetes_namespace_v1.task.metadata[0].name }
-  spec { pod_selector {}; policy_types = ["Ingress"] }
+  metadata { name = "default-deny-ingress"
+  namespace = kubernetes_namespace_v1.task.metadata[0].name }
+  spec { pod_selector {}
+  policy_types = ["Ingress"] }
 }
