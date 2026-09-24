0ad442d85c32443f8148b6972e1e13342c84ee36
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-25 08:22:00 +0900

    Add terraform/10-network-host/variables.tf

---
+++ terraform/10-network-host/variables.tf
@@ -0,0 +1,12 @@
+variable "host_project_id" { type = string; default = "gcp-prod-edp-hub-vpchost" }
+variable "service_project_id" { type = string; default = "gcp-sbx-edp-gke01" }
+variable "region" { type = string; default = "asia-northeast3" }
+variable "network_name" { type = string; default = "vpc-prod-edp-hub" }
+variable "admin_subnet_cidr" { type = string; default = "172.31.10.0/24" }
+variable "alb_frontend_cidr" { type = string; default = "172.31.96.0/27" }
+variable "alb_frontend_ip" { type = string; default = "172.31.96.10" }
+variable "alb_proxy_cidr" { type = string; default = "10.252.2.0/26" }
+variable "gke_node_cidr" { type = string; default = "10.252.0.0/24" }
+variable "gke_pod_cidr" { type = string; default = "10.240.0.0/20" }
+variable "cloudrun_cidr" { type = string; default = "10.251.0.0/26" }
+variable "cloudbuild_psa_cidr" { type = string; default = "10.250.0.0/24" }
