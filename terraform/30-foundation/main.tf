7f912de0719b276719c0954e61b312f132ff3a14
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-25 08:23:01 +0900

    Add terraform/30-foundation/main.tf

---
+++ terraform/30-foundation/main.tf
@@ -0,0 +1,76 @@
+locals {
+  services = toset([
+    "artifactregistry.googleapis.com", "cloudbuild.googleapis.com", "run.googleapis.com",
+    "container.googleapis.com", "config.googleapis.com", "workflows.googleapis.com",
+    "workflowexecutions.googleapis.com", "secretmanager.googleapis.com"
+  ])
+}
+
+resource "google_project_service" "foundation" {
+  for_each           = local.services
+  project            = var.platform_project_id
+  service            = each.value
+  disable_on_destroy = false
+}
+
+data "google_compute_network" "shared" {
+  project = var.host_project_id
+  name    = var.network_name
+}
+
+data "google_compute_subnetwork" "gke" {
+  project = var.host_project_id
+  region  = var.region
+  name    = var.gke_subnet_name
+}
+
+resource "google_artifact_registry_repository" "platform" {
+  project       = var.platform_project_id
+  location      = var.region
+  repository_id = var.artifact_repository
+  format        = "DOCKER"
+  depends_on    = [google_project_service.foundation]
+}
+
+resource "google_storage_bucket" "requests" {
+  project                     = var.platform_project_id
+  name                        = var.request_bucket_name
+  location                    = var.region
+  uniform_bucket_level_access = true
+  public_access_prevention    = "enforced"
+  versioning { enabled = true }
+}
+
+resource "google_storage_bucket" "bundles" {
+  project                     = var.platform_project_id
+  name                        = var.bundle_bucket_name
+  location                    = var.region
+  uniform_bucket_level_access = true
+  public_access_prevention    = "enforced"
+  versioning { enabled = true }
+}
+
+resource "google_container_cluster" "main" {
+  provider = google
+  project  = var.platform_project_id
+  name     = var.cluster_name
+  location = var.region
+  network    = data.google_compute_network.shared.self_link
+  subnetwork = data.google_compute_subnetwork.gke.self_link
+  enable_autopilot = true
+  deletion_protection = true
+
+  ip_allocation_policy {
+    cluster_secondary_range_name = var.gke_pod_range_name
+    services_ipv4_cidr_block     = var.gke_service_cidr
+  }
+
+  private_cluster_config {
+    enable_private_nodes    = true
+    enable_private_endpoint = true
+    master_ipv4_cidr_block  = var.control_plane_cidr
+  }
+
+  release_channel { channel = "REGULAR" }
+  depends_on = [google_project_service.foundation]
+}
