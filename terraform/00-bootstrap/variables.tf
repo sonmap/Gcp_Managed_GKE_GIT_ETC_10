30870af4f6bb61545ef6ebc5f1eebc144ff13807
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-25 08:21:58 +0900

    Add terraform/00-bootstrap/variables.tf

---
+++ terraform/00-bootstrap/variables.tf
@@ -0,0 +1,3 @@
+variable "platform_project_id" { type = string; default = "gcp-sbx-edp-gke01" }
+variable "region" { type = string; default = "asia-northeast3" }
+variable "state_bucket_name" { type = string; default = "gcp-sbx-edp-gke01-tfstate" }
