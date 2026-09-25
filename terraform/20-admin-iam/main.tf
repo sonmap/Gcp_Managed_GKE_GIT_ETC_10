8007c1de2fe97ca4392e006efd9ec1d4338ba844
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-25 23:50:18 +0900

    fix(iam): allow foundation to read GKE instance groups

--- terraform/20-admin-iam/main.tf
+++ terraform/20-admin-iam/main.tf
@@ -23,7 +23,7 @@ locals {
       "roles/container.admin", "roles/run.admin", "roles/cloudbuild.builds.editor",
       "roles/artifactregistry.admin", "roles/workflows.admin", "roles/storage.admin",
       "roles/secretmanager.admin", "roles/iam.serviceAccountAdmin", "roles/serviceusage.serviceUsageAdmin",
-      "roles/config.agent"
+      "roles/config.agent", "roles/compute.viewer"
     ])
     project_factory = toset(["roles/config.agent"])
     gke_admin       = toset(["roles/container.admin"])
