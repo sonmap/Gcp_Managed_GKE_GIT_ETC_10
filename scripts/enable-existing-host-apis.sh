80e376cdb05bb4c219d1024ae88bc18c61d97da4
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-25 23:25:08 +0900

    Enable GKE API and service identity in existing host project

--- scripts/enable-existing-host-apis.sh
+++ scripts/enable-existing-host-apis.sh
@@ -6,8 +6,13 @@ HOST_PROJECT_ID="${1:-gcp-prod-edp-hub-vpchost}"
 gcloud services enable \
   cloudresourcemanager.googleapis.com \
   compute.googleapis.com \
+  container.googleapis.com \
   iam.googleapis.com \
   iamcredentials.googleapis.com \
   serviceusage.googleapis.com \
   servicenetworking.googleapis.com \
+  --project="${HOST_PROJECT_ID}"
+
+gcloud beta services identity create \
+  --service=container.googleapis.com \
   --project="${HOST_PROJECT_ID}"
