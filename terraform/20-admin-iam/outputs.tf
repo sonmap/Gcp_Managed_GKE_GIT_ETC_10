f7a839c66a6ff9c2270062fd394309c0f11620da
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-25 21:38:51 +0900

    Expose Infrastructure Manager caller IAM outputs

--- terraform/20-admin-iam/outputs.tf
+++ terraform/20-admin-iam/outputs.tf
@@ -1,1 +1,21 @@
-output "service_accounts" { value = { for k, v in google_service_account.automation : k => v.email } }
+output "service_accounts" {
+  value = {
+    for key, service_account in google_service_account.automation :
+    key => service_account.email
+  }
+}
+
+output "vm_service_account" {
+  description = "instance-son VM service account used to call Infrastructure Manager."
+  value       = var.vm_service_account
+}
+
+output "vm_infrastructure_manager_role" {
+  description = "Project role granted to the VM service account."
+  value       = google_project_iam_member.vm_infrastructure_manager_admin.role
+}
+
+output "foundation_service_account" {
+  description = "Execution service account selected by Infrastructure Manager."
+  value       = google_service_account.automation["im_foundation"].email
+}
