693e57101c6de0bd31df6ab8e2dc957e01e8e7be
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-25 20:24:31 +0900

    Refactor variable declarations in variables.tf

--- terraform/30-foundation/variables.tf
+++ terraform/30-foundation/variables.tf
@@ -1,24 +1,48 @@
-variable "platform_project_id" { type = string
-  default = "gcp-sbx-edp-gke01" }
-variable "host_project_id" { type = string
-  default = "gcp-prod-edp-hub-vpchost" }
-variable "region" { type = string
-  default = "asia-northeast3" }
-variable "network_name" { type = string
-  default = "vpc-prod-edp-hub" }
-variable "gke_subnet_name" { type = string
-  default = "subnet-prod-edp-gke-an3" }
-variable "gke_pod_range_name" { type = string
-  default = "pods-prod-edp-gke-an3" }
-variable "gke_service_cidr" { type = string
-  default = "34.118.224.0/20" }
-variable "control_plane_cidr" { type = string
-  default = "10.253.0.0/28" }
-variable "cluster_name" { type = string
-  default = "gke-sbx-edp-main-an3" }
-variable "artifact_repository" { type = string
-  default = "ar-sbx-platform" }
-variable "request_bucket_name" { type = string
-  default = "gcp-sbx-edp-gke01-requests" }
-variable "bundle_bucket_name" { type = string
-  default = "gcp-sbx-edp-gke01-bundles" }
+variable "platform_project_id" { 
+  type = string
+  default = "gcp-sbx-edp-gke01" 
+}
+variable "host_project_id" { 
+  type = string
+  default = "gcp-prod-edp-hub-vpchost" 
+}
+variable "region" { 
+  type = string
+  default = "asia-northeast3" 
+}
+variable "network_name" { 
+  type = string
+  default = "vpc-prod-edp-hub" 
+}
+variable "gke_subnet_name" { 
+  type = string
+  default = "subnet-prod-edp-gke-an3" 
+}
+variable "gke_pod_range_name" { 
+  type = string
+  default = "pods-prod-edp-gke-an3" 
+}
+variable "gke_service_cidr" { 
+  type = string
+  default = "34.118.224.0/20" 
+}
+variable "control_plane_cidr" { 
+  type = string
+  default = "10.253.0.0/28" 
+}
+variable "cluster_name" { 
+  type = string
+  default = "gke-sbx-edp-main-an3" 
+}
+variable "artifact_repository" { 
+  type = string
+  default = "ar-sbx-platform" 
+}
+variable "request_bucket_name" { 
+  type = string
+  default = "gcp-sbx-edp-gke01-requests" 
+}
+variable "bundle_bucket_name" { 
+  type = string
+  default = "gcp-sbx-edp-gke01-bundles" 
+}
