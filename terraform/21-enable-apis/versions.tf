1f3ed81df88c16c481299ae09703ecc2a9a173c7
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-25 23:17:24 +0900

    Add Google beta provider for service identities

--- terraform/21-enable-apis/versions.tf
+++ terraform/21-enable-apis/versions.tf
@@ -1,10 +1,17 @@
 terraform {
   required_version = ">= 1.5.7"
+
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "~> 6.0"
     }
+
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 6.0"
+    }
   }
+
   backend "gcs" {}
 }
