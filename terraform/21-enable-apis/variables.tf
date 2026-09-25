672b83404857d7d164f39324def9b71b7c7ae2ff
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-25 22:00:23 +0900

    Remove existing host project from stage 21 variables

--- terraform/21-enable-apis/variables.tf
+++ terraform/21-enable-apis/variables.tf
@@ -1,15 +1,13 @@
-variable "host_project_id" {
-  type    = string
-  default = "gcp-prod-edp-hub-vpchost"
-}
 variable "platform_project_id" {
   type    = string
   default = "gcp-sbx-edp-gke01"
 }
+
 variable "data_project_id" {
   type    = string
   default = "gcp-sbx-edp-comn-509423"
 }
+
 variable "region" {
   type    = string
   default = "asia-northeast3"
