69dd2645ced4dfc19ba83a9d5ec5a99e43074096
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-25 21:56:16 +0900

    Move task API activation to stage 21

--- terraform/40-task/10-project/main.tf
+++ terraform/40-task/10-project/main.tf
@@ -23,21 +23,10 @@ provider "google" {
   region  = var.region
 }
 
-locals {
-  services = toset([
-    "bigquery.googleapis.com",
-    "storage.googleapis.com",
-    "iam.googleapis.com"
-  ])
-}
-
-resource "google_project_service" "task" {
-  for_each           = local.services
-  project            = var.project_id
-  service            = each.value
-  disable_on_destroy = false
+data "google_project" "task" {
+  project_id = var.project_id
 }
 
 output "project_id" {
-  value = var.project_id
+  value = data.google_project.task.project_id
 }
