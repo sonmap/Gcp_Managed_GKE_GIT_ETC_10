3bbe4e00a55b060f0b4c11692b8c2b6ffcbba7c5
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-26 16:13:15 +0900

    feat(stage40): automate Infrastructure Manager and private GKE

--- terraform/30-foundation/automation.tf
+++ terraform/30-foundation/automation.tf
@@ -14,6 +14,15 @@ data "google_service_account" "provisioner" {
   account_id = "sa-sbx-provisioner"
 }
 
+data "google_service_account" "workflow" {
+  project    = var.platform_project_id
+  account_id = "sa-sbx-workflow"
+}
+
+data "google_storage_project_service_account" "gcs" {
+  project = var.platform_project_id
+}
+
 resource "google_cloud_run_v2_service" "provisioner" {
   count    = var.enable_automation_resources ? 1 : 0
   project  = var.platform_project_id
@@ -23,12 +32,86 @@ resource "google_cloud_run_v2_service" "provisioner" {
 
   template {
     service_account = data.google_service_account.provisioner.email
+    timeout         = "3600s"
+
+    vpc_access {
+      network_interfaces {
+        network    = "projects/${var.host_project_id}/global/networks/${var.network_name}"
+        subnetwork = "projects/${var.host_project_id}/regions/${var.region}/subnetworks/${var.cloudrun_subnet_name}"
+      }
+      egress = "PRIVATE_RANGES_ONLY"
+    }
+
     containers {
       image = var.provisioner_image
       env {
         name  = "REQUEST_BUCKET"
         value = google_storage_bucket.requests.name
       }
+      env {
+        name  = "PLATFORM_PROJECT_ID"
+        value = var.platform_project_id
+      }
+      env {
+        name  = "REGION"
+        value = var.region
+      }
+      env {
+        name  = "IM_SERVICE_ACCOUNT"
+        value = "sa-sbx-build@${var.platform_project_id}.iam.gserviceaccount.com"
+      }
+      env {
+        name  = "GKE_ADMIN_SERVICE_ACCOUNT"
+        value = "sa-im-gke-admin@${var.platform_project_id}.iam.gserviceaccount.com"
+      }
     }
   }
+}
+
+resource "google_project_iam_member" "gcs_eventarc_publisher" {
+  count   = var.enable_automation_resources ? 1 : 0
+  project = var.platform_project_id
+  role    = "roles/pubsub.publisher"
+  member  = "serviceAccount:${data.google_storage_project_service_account.gcs.email_address}"
+}
+
+resource "google_cloud_run_v2_service_iam_member" "eventarc_invoker" {
+  count    = var.enable_automation_resources ? 1 : 0
+  project  = var.platform_project_id
+  location = var.region
+  name     = google_cloud_run_v2_service.provisioner[0].name
+  role     = "roles/run.invoker"
+  member   = "serviceAccount:${data.google_service_account.workflow.email}"
+}
+
+resource "google_eventarc_trigger" "approved_request" {
+  count    = var.enable_automation_resources ? 1 : 0
+  project  = var.platform_project_id
+  name     = "trigger-sbx-approved-request"
+  location = var.region
+
+  matching_criteria {
+    attribute = "type"
+    value     = "google.cloud.storage.object.v1.finalized"
+  }
+
+  matching_criteria {
+    attribute = "bucket"
+    value     = google_storage_bucket.requests.name
+  }
+
+  destination {
+    cloud_run_service {
+      service = google_cloud_run_v2_service.provisioner[0].name
+      region  = var.region
+      path    = "/events/storage"
+    }
+  }
+
+  service_account = data.google_service_account.workflow.email
+
+  depends_on = [
+    google_project_iam_member.gcs_eventarc_publisher,
+    google_cloud_run_v2_service_iam_member.eventarc_invoker
+  ]
 }
