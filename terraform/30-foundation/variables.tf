8761802e2c16ea7cd7fbedcd640de2b1c46d8475
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-26 22:16:13 +0900

    chore(run): force Jupyter resource revision

--- terraform/30-foundation/variables.tf
+++ terraform/30-foundation/variables.tf
@@ -65,6 +65,6 @@ variable "cloudrun_subnet_name" {
 
 variable "provisioner_release" {
   type        = string
-  default     = "20260926-jupyterhub2"
+  default     = "20260926-jupyterhub3"
   description = "Changes when a new provisioner image must create a Cloud Run revision."
 }
