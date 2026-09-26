ce8ac73c942048fc08d379f287c8bf982614a0b5
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-26 21:19:24 +0900

    chore(run): force mirrored JupyterHub revision

--- terraform/30-foundation/variables.tf
+++ terraform/30-foundation/variables.tf
@@ -65,6 +65,6 @@ variable "cloudrun_subnet_name" {
 
 variable "provisioner_release" {
   type        = string
-  default     = "20260926-jupyterhub1"
+  default     = "20260926-jupyterhub2"
   description = "Changes when a new provisioner image must create a Cloud Run revision."
 }
